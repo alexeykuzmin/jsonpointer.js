a383c1404a3cc72ca397e0b5a4439f47454d55f1
Alexey Kuzmin <alex.s.kuzmin@gmail.com>
2013-04-09 03:07:26 +0400

    Add test environment and stub implementation

---
+++ Gruntfile.js
@@ -0,0 +1,63 @@
+module.exports = function(grunt) {
+  'use strict';
+
+  grunt.loadNpmTasks('grunt-coffeelint');
+  grunt.loadNpmTasks('grunt-contrib-coffee');
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-simple-mocha');
+
+
+  grunt.initConfig({
+
+    coffee: {
+      test: {
+        expand: true,
+        cwd: 'test',
+        src: ['*.coffee'],
+        dest: 'test',
+        ext: '.compiled.js'
+      }
+    },
+
+
+    coffeelint: {
+      options: {
+        'no_trailing_whitespace': {
+          'level': 'error'
+        }
+      },
+
+      test: ['test/test-*.coffee']
+    },
+
+
+    jshint: {
+      options: {
+        jshintrc: '.jshintrc'
+      },
+
+      gruntfile: 'Gruntfile.js',
+      lib: ['lib/*.js']
+    },
+
+
+    simplemocha: {
+      options: {
+        ignoreLeaks: false,
+        ui: 'bdd',
+        reporter: 'dot'
+      },
+
+      test: {
+        src: 'test/test-*.js'
+      }
+    }
+  });
+
+
+  grunt.registerTask('check_codestyle', ['jshint', 'coffeelint']);
+  grunt.registerTask('run_tests', ['coffee', 'simplemocha']);
+  grunt.registerTask('test', ['check_codestyle', 'run_tests']);
+  grunt.registerTask('default', 'test');
+
+};
---
+++ test/test-js-json-pointer.coffee
@@ -0,0 +1,44 @@
+
+jsonPointer = require "../lib/js-json-pointer"
+chai = require "chai"
+chai.should()
+
+
+describe "jsonPointer", () ->
+
+  describe "#get", () ->
+
+    it "should evaluate spec examples", () ->
+      target =
+        "foo": ["bar", "baz"]
+        "": 0
+        "a/b": 1,
+        "c%d": 2,
+        "e^f": 3,
+        "g|h": 4,
+        "i\\j": 5,
+        "k\"l": 6,
+        " ": 7,
+        "m~n": 8
+
+      targetAsString = JSON.stringify target
+
+      specExamples =
+        "": target
+        "/foo": ["bar", "baz"],
+        "/foo/0": "bar",
+        "/": 0,
+        "/a~1b": 1,
+        "/c%d": 2,
+        "/e^f": 3,
+        "/g|h": 4,
+        "/i\\j": 5,
+        "/k\"l": 6,
+        "/ ": 7,
+        "/m~0n": 8
+
+      check = (expression, expected) ->
+        actual = jsonPointer.get targetAsString, expression
+        actual.should.deep.equal expected
+
+      check(expression, expected) for expression, expected of specExamples
